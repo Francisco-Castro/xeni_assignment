fba19336cb5f041a1ddf10c11fc6a22ec596be70
Francisco Castro <catrapato@hotmail.com>
2023-08-16 16:30:07 -0400

    Remove required timestamp

--- lib/xeni/schemas/record.ex
+++ lib/xeni/schemas/record.ex
@@ -2,16 +2,16 @@ defmodule Xeni.Schemas.Record do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @derive {Jason.Encoder, only: [:open, :high, :low, :close, :timestamp]}
+  @derive {Jason.Encoder, only: [:id, :open, :high, :low, :close, :timestamp]}
   schema "records" do
     field :open, :float
     field :high, :float
     field :low, :float
     field :close, :float
-    field :timestamp, :utc_datetime_usec
+    field :timestamp, :utc_datetime_usec, default: Timex.now()
   end
 
-  @required_fields ~w(open high low close timestamp)a
+  @required_fields ~w(open high low close)a
   @optional_fields ~w()a
 
   def changeset(record, attrs) do
